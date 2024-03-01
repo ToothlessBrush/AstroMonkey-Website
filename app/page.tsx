1477de7f9798c384cde72a7004e5c81d4148afbe
Brendan <Chermackbrendan@gmail.com>
2024-03-01 02:04:38 -0600

    bg work

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import { Footer } from '../components/footer';
-import { NavBar } from '../components/navbar';
+// import styles from './star-field.css';
+// import { Footer } from '../components/footer';
+// import { NavBar } from '../components/navbar';
 
-export default function Page() {
+// export default function Page() {
+// 	return (
+// 		<div>
+// 			{/* <NavBar /> */}
+//             <div className={'bg'}>
+// 				<div className='star-field'>
+// 					<div className='layer'></div>
+// 					<div className='layer'></div>
+// 					<div className='layer'></div>
+// 				</div>
+// 			</div>
+// 			{/* <Footer /> */}
+// 		</div>
+// 	);
+// }
+import styles from '../styles/star-field.module.css';
+
+export default function Home() {
 	return (
-		<div>
-			<NavBar />
-			<Footer />
+		<div className={styles.customBody}>
+			<div className={styles.customBody}>
+				<div className={styles.bg}></div>
+
+				<div className={styles['star-field']}>
+					<div className={styles.layer}></div>
+					<div className={styles.layer}></div>
+					<div className={styles.layer}></div>
+				</div>
+			</div>
 		</div>
 	);
 }
