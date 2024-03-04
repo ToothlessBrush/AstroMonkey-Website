e6d7f2e97cbe7ece2fb7e85f857c33936e69ecb0
Brendan <Chermackbrendan@gmail.com>
2024-03-04 01:25:08 -0600

    i forgor 2

--- app/contact-us/layout.tsx
+++ app/contact-us/layout.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
-const ContactUseLayout = () => {
-  return (
-    <div>ContactUseLayout</div>
-  )
-}
+import styles from '@/styles/faq-bg.module.css';
+import { Footer } from '@/components/footer';
+import { NavBar } from '@/components/navbar';
+const ContactUseLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className={styles.bg}>
+			<NavBar />
+            <main className=''>{children}</main>
+            <Footer />
+		</div>
+	);
+};
 
 export default ContactUseLayout
--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactUsPage = () => {
   return (
-    <div>ContactUsPage</div>
+    <div className='text-white'>ContactUsPage</div>
   )
 }
 
